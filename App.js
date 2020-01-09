98fb4cf2ca9e059b0a7ac6600210eec6483ffa9a
sotomaque <sotomaque@gmail.com>
2020-01-09 11:39:10 -0800

    added background image / logic to preload them so we can avoid flickering effect

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { Asset } from 'expo-asset';
+import { AppLoading } from 'expo';
+
+import MusicApp from './app/index';
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    }
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([require('./assets/bg.jpg')]);
+
+    await Promise.all([...imageAssets]);
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <MusicApp />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
